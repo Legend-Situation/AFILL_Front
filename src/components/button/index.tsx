0fba2703b369f5e02277a16aa6d10b45b557f0be
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-10-17 00:42:10 +0900

    design : 버튼 컴포넌트 스타일 수정

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import * as s from './style.css';
+import React from "react";
+import * as s from "./style.css";
+import classNames from "classnames"; // classnames 라이브러리 사용
 
 type ButtonProps = {
   text: string;
-}
-
-const AddExperienceButton = ({ text }: ButtonProps) => {
-  return (
-    <button className={s.Button}>
-      {text}
-    </button>
-  );
+  color: "gray" | "blue"; // color 속성 추가
 };
 
-export default AddExperienceButton;
+const Button = ({ text, color }: ButtonProps) => {
+  const buttonClass = classNames(s.Button, {
+    [s.grayButton]: color === "gray",
+    [s.blueButton]: color === "blue",
+  });
+
+  return <button className={buttonClass}>{text}</button>;
+};
+
+export default Button;
