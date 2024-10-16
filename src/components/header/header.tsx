cf03d08893a8f7652f52e21891ed2a506c33f272
rkdalswl718 <rkdalswl070718@gmail.com>
2024-10-16 13:52:21 +0900

    add :: map

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
-import { Logo } from '@/ui/src/assets';
-import * as s from './style.css';
+"use client"
+
+import React from "react";
+import { Logo } from "@/ui/src/assets";
+import * as s from "./style.css";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const navItems = [
+    {
+      name: "경험카드",
+      href: "/experience-card",
+    },
+    {
+      name: "자기소개서",
+      href: "/self-introduction",
+    },
+  ];
+
   return (
     <header className={s.container}>
-        <Logo width={96} />
-      <div className={s.menu}>
-        <a href="#" className={`${s.menuItem} ${s.active}`}>경험카드</a>
-        <a href="#" className={s.menuItem}>자기소개서</a>
-      </div>
+      <Logo width={96} />
+      <nav className={s.menu}>
+        {navItems.map((item) => (
+          <div
+            key={item.href}
+            className={`${s.menuItem} ${pathname === item.href ? s.active : ""}`}
+            onClick={() => router.push(item.href)}
+          >
+            {item.name}
+          </div>
+        ))}
+      </nav>
       <div className={s.profile}>
-        <span>강민지님</span>
+        <p>강민지님</p>
         <div className={s.profileIcon}></div>
       </div>
     </header>
