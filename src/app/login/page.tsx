8c27cb02d2a8d4f74c08d4c3dd307dc8f8a106a4
rkdalswl718 <rkdalswl070718@gmail.com>
2024-10-17 03:06:25 +0900

    add :: auth api

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,21 @@
+'use client';
+
 import React from "react";
 import * as s from "./style.css";
 import { Logo } from "@ui/src/assets";
 import { Google } from "@ui/src/icons";
 
 const Login = () => {
-  const onGoogleLogin = () => {
-    window.location.href = "https://afill.legend-situation.kro.kr/api/auth/google";
+  const handleLogin = () => {
+    window.location.href = "https://afill.legend-situation.kro.kr/auth/google";
   };
+
   return (
     <div className={s.Container}>
       <p className={s.Description}>
         지긋지긋한 자기소개서, 어필로 시작해보세요.
       </p>
-      <button className={s.GoogleButton} onClick={onGoogleLogin}>
+      <button className={s.GoogleButton} onClick={handleLogin}>
         <Google />
         <span className={s.GoogleButtonText}>Google로 로그인</span>
       </button>
